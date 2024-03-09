9d200491010dcb81f53c622c5cb459537a8cd3e2
damm13g11@gmail.com <dam.m13.g11@gmail.com>
2024-03-09 19:55:35 +0100

    Actualizacion backend

--- app/src/main/java/com/example/m13/db/DbCervezas.java
+++ app/src/main/java/com/example/m13/db/DbCervezas.java
@@ -20,7 +20,7 @@ public class DbCervezas extends DbHelper {
         this.context = context;
     }
 
-    public long insertarCerveza(String nombre, String pais, String tipo, String marca, int precio) {
+    public long insertarCerveza(String nombre, String pais, String tipo, String marca, double precio, double graduacion) {
 
         long id = 0;
 
@@ -34,6 +34,7 @@ public class DbCervezas extends DbHelper {
             values.put("tipo", tipo);
             values.put("marca", marca);
             values.put("precio", precio);
+            values.put("graduacion", graduacion);
 
             id = db.insert(TABLE_CERVEZAS, null, values);
 
@@ -61,7 +62,92 @@ public class DbCervezas extends DbHelper {
                 cerveza.setPais(cursorCervezas.getString(2));
                 cerveza.setTipo(cursorCervezas.getString(3));
                 cerveza.setMarca(cursorCervezas.getString(4));
-                cerveza.setPrecio(cursorCervezas.getInt(5));
+                cerveza.setPrecio(cursorCervezas.getDouble(5));
+                cerveza.setGraduacion(cursorCervezas.getDouble(6));
+
+                listaCervezas.add(cerveza);
+            } while (cursorCervezas.moveToNext());
+        }
+        cursorCervezas.close();
+
+        return listaCervezas;
+    }
+
+    public ArrayList<Cerveza> getCervezasPorPais(String pais) {
+        DbHelper dbHelper = new DbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ArrayList<Cerveza> listaCervezas = new ArrayList<>();
+        Cursor cursorCervezas = null;
+
+        cursorCervezas = db.rawQuery("SELECT * FROM " + TABLE_CERVEZAS + " WHERE pais=?", new String[]{pais});
+
+        if (cursorCervezas.moveToFirst()) {
+            do {
+                Cerveza cerveza = new Cerveza();
+                cerveza.setId(cursorCervezas.getInt(0));
+                cerveza.setNombre(cursorCervezas.getString(1));
+                cerveza.setPais(cursorCervezas.getString(2));
+                cerveza.setTipo(cursorCervezas.getString(3));
+                cerveza.setMarca(cursorCervezas.getString(4));
+                cerveza.setPrecio(cursorCervezas.getDouble(5));
+                cerveza.setGraduacion(cursorCervezas.getDouble(6));
+
+                listaCervezas.add(cerveza);
+            } while (cursorCervezas.moveToNext());
+        }
+        cursorCervezas.close();
+
+        return listaCervezas;
+    }
+
+    public ArrayList<Cerveza> getCervezasPorTipo(String tipo) {
+        DbHelper dbHelper = new DbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ArrayList<Cerveza> listaCervezas = new ArrayList<>();
+        Cursor cursorCervezas = null;
+
+        cursorCervezas = db.rawQuery("SELECT * FROM " + TABLE_CERVEZAS + " WHERE tipo=?", new String[]{tipo});
+
+        if (cursorCervezas.moveToFirst()) {
+            do {
+                Cerveza cerveza = new Cerveza();
+                cerveza.setId(cursorCervezas.getInt(0));
+                cerveza.setNombre(cursorCervezas.getString(1));
+                cerveza.setPais(cursorCervezas.getString(2));
+                cerveza.setTipo(cursorCervezas.getString(3));
+                cerveza.setMarca(cursorCervezas.getString(4));
+                cerveza.setPrecio(cursorCervezas.getDouble(5));
+                cerveza.setGraduacion(cursorCervezas.getDouble(6));
+
+                listaCervezas.add(cerveza);
+            } while (cursorCervezas.moveToNext());
+        }
+        cursorCervezas.close();
+
+        return listaCervezas;
+    }
+
+    public ArrayList<Cerveza> getCervezasPorMarca(String marca) {
+        DbHelper dbHelper = new DbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ArrayList<Cerveza> listaCervezas = new ArrayList<>();
+        Cursor cursorCervezas = null;
+
+        cursorCervezas = db.rawQuery("SELECT * FROM " + TABLE_CERVEZAS + " WHERE marca=?", new String[]{marca});
+
+        if (cursorCervezas.moveToFirst()) {
+            do {
+                Cerveza cerveza = new Cerveza();
+                cerveza.setId(cursorCervezas.getInt(0));
+                cerveza.setNombre(cursorCervezas.getString(1));
+                cerveza.setPais(cursorCervezas.getString(2));
+                cerveza.setTipo(cursorCervezas.getString(3));
+                cerveza.setMarca(cursorCervezas.getString(4));
+                cerveza.setPrecio(cursorCervezas.getDouble(5));
+                cerveza.setGraduacion(cursorCervezas.getDouble(6));
 
                 listaCervezas.add(cerveza);
             } while (cursorCervezas.moveToNext());
@@ -88,52 +174,40 @@ public class DbCervezas extends DbHelper {
             cerveza.setPais(cursorCervezas.getString(2));
             cerveza.setTipo(cursorCervezas.getString(3));
             cerveza.setMarca(cursorCervezas.getString(4));
-            cerveza.setPrecio(cursorCervezas.getInt(5));
+            cerveza.setPrecio(cursorCervezas.getDouble(5));
+            cerveza.setGraduacion(cursorCervezas.getDouble(6));
         }
 
         cursorCervezas.close();
 
         return cerveza;
     }
-    public boolean editarCerveza(int id, String nombre, String pais, String tipo, String marca, int precio) {
 
-        boolean correcto = false;
-
+    public void editarCerveza(int id, String nombre, String pais, String tipo, String marca, double precio, double graduacion) {
         DbHelper dbHelper = new DbHelper(context);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         try {
             db.execSQL("UPDATE " + TABLE_CERVEZAS + " SET nombre = '" + nombre + "', pais = '" + pais + "', tipo = '" + tipo +
-                    "', marca = '" + marca + "', precio = '" + precio + "' WHERE id='" + id + "' ");
-            correcto = true;
+                    "', marca = '" + marca + "', precio = '" + precio + "', graduacion = '" + graduacion + "' WHERE id='" + id + "' ");
         } catch (Exception ex) {
             ex.toString();
-            correcto = false;
         } finally {
             db.close();
         }
-
-        return correcto;
     }
 
-    public boolean eliminarCerveza(int id) {
-
-        boolean correcto = false;
-
+    public void eliminarCerveza(int id) {
         DbHelper dbHelper = new DbHelper(context);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         try {
             db.execSQL("DELETE FROM " + TABLE_CERVEZAS + " WHERE id = '" + id + "'");
-            correcto = true;
         } catch (Exception ex) {
             ex.toString();
-            correcto = false;
         } finally {
             db.close();
         }
-
-        return correcto;
     }
 
 }
